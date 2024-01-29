9a472cf1b9204e6bd45c0045be5605f4ec499637
GerardMaidana2024 <157707876+GerardMaidana2024@users.noreply.github.com>
2024-01-29 14:23:54 +0100

    Error de codigo 01

--- 001 Cicle de  vida d'un programa/Activitat_03/Activitat_03.ino
+++ 001 Cicle de  vida d'un programa/Activitat_03/Activitat_03.ino
@@ -1,13 +1,13 @@
 
 void setup() {
   // initialize digital pin LED_BUILTIN as an output.
-  pinMode(LED_BUILTIN, OUTPUT);
+  pinMode(13, OUTPUT);
 }
 
 // the loop function runs over and over again forever
 void loop() {
-  digitalWrite(LED_BUILTIN, HIGH);  // 
+  digitalWrite(13, HIGH);  // 
   delay(60);                      
-  digitalWrite(LED_BUILTIN, LOW);   // Espacio entre latidos
+  digitalWrite(13, LOW);   // Espacio entre latidos
   delay(60);                      
 }
