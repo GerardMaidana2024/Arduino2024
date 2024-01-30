7dab8d8f25c51f66cd77b62cb304c74093ed5989
GerardMaidana2024 <157707876+GerardMaidana2024@users.noreply.github.com>
2024-01-30 09:38:22 +0100

    primer codigo0001

---
+++ 003 Tipus de dades i operadors/003-01/003-01.ino
@@ -0,0 +1,40 @@
+int T  =  9;
+int Y  =  4;
+int K  =  8;
+int N  =  3;
+int P  = - 3;
+boolean Z  = true;
+boolean W = false;
+boolean A  =  false;
+boolean B  =  true;
+
+void setup() {
+  Serial.begin(9600);
+  delay(2000);
+  Serial.println("a. Afirmacio 1: (T > 10 && T < 14) = ");
+          if (T > 10 && T < 14); // Afirmacio 1
+              Serial.print("boolean A" or "boolean B");
+  Serial.println("b. Afirmacio 2: (Y > 4 || Y < 7)=");
+          if ((Y > 4 || Y < 7)); // Afirmacio 2
+              Serial.print("boolean A" or "boolean B");
+  Serial.println()
+  if ((K > 8 && K < 10)); // Afirmacio 3
+  Serial.print("boolean A" or "boolean B");
+  if ((N > 2 || N < 20)); // Afirmacio 4
+  Serial.print("boolean A" or "boolean B");
+  if ((P > 0 && P < -3)); // Afirmacio 5
+  Serial.print("boolean A" or "boolean B");
+  if (T > 0 || Z); // Afirmacio 6
+  Serial.print("boolean A" or "boolean B");
+  if (P > 0 && B); // Afirmacio 7
+  Serial.print("boolean A" or "boolean B");
+  if (W && B); // Afirmacio 8
+  Serial.print("boolean A" or "boolean B");
+  if (W && A); // Afirmacio 9
+  Serial.print("boolean A" or "boolean B");
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
