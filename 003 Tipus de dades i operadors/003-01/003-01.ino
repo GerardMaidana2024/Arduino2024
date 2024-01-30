ee269e21a516278019ac312afb2f9aec94959351
GerardMaidana2024 <157707876+GerardMaidana2024@users.noreply.github.com>
2024-01-30 17:41:20 +0100

    Codigo final 003-01

--- 003 Tipus de dades i operadors/003-01/003-01.ino
+++ 003 Tipus de dades i operadors/003-01/003-01.ino
@@ -11,55 +11,87 @@ boolean B  =  true;
 void setup() {
   Serial.begin(9600);
   delay(2000);
-  Serial.println("a. Afirmacio 1: (T > 10 && T < 14) = ");
-          if (T > 10 && T < 14) {
-            Serial.print("true");
+  Serial.print("a. Afirmacio 1: (T > 10 && T < 14) = ");
+          if (T > 10 && T < 14) 
+          {
+            Serial.println("true");
           }                                                      // Afirmacio 1
-          else {
-            Serial.print("false");
+          else 
+          {
+            Serial.println("false");
           }
-  Serial.println("b. Afirmacio 2: (Y > 4 || Y < 7)=");
-          if ((Y > 4 || Y < 7)) {
-            Serial.print("true");
+  Serial.print("b. Afirmacio 2: (Y > 4 || Y < 7)= ");
+          if ((Y > 4 || Y < 7)) 
+          {
+            Serial.println("true");
           }                                                     // Afirmacio 2
-          else {
-            Serial.print("false");
+          else 
+          {
+            Serial.println("false");
           }
-  Serial.println("c. Afirmacio 3: (K > 8 && K < 10) =");
-          if ((K > 8 && K < 10))
-            Serial.print("true");
+  Serial.print("c. Afirmacio 3: (K > 8 && K < 10) = ");
+          if ((K > 8 && K < 10)) 
+          {
+            Serial.println("true");
           }                                                     // Afirmacio 3
-          else {
-            Serial.print("false");
+          else 
+          {
+            Serial.println("false");
           }
-  Serial.println("d. Afirmacio 4: (N > 2 || N < 20) =");
-          if ((N > 2 || N < 20))
-            Serial.print("true");
+  Serial.print("d. Afirmacio 4: (N > 2 || N < 20) = ");
+          if ((N > 2 || N < 20)) 
+          {
+            Serial.println("true");
           }                                                     // Afirmacio 4
-          else {
-            Serial.print("false");
+          else 
+          {
+            Serial.println("false");
           }
-  Serial.println("e. Afirmacio 5: (N > 2 || N < 20) =");
-          if ((P > 0 && P < -3))
-            Serial.print("true");
+  Serial.print("e. Afirmacio 5: (N > 2 || N < 20) = ");
+          if ((P > 0 && P < -3)) 
+          {
+            Serial.println("true");
           }                                                     // Afirmacio 5
-          else {
-            Serial.print("false");
+          else 
+          {
+            Serial.println("false");
           }
-  Serial.println("f. Afirmacio 6: (T > 10 || Z) =")
-          if (T > 0 || Z)
-            Serial.print("true");
+Serial.print("f. Afirmacio 6: (T > 10 || Z) = ");
+          if(T > 0 || Z) 
+          {
+            Serial.println("true");
           }                                                     // Afirmacio 6
-          else {
-            Serial.print("false");
+          else 
+          {
+            Serial.println("false");
           }
-  Serial.println("g. Afirmacio 7: (P")
-  if (P > 0 && B); // Afirmacio 7
-  Serial.print("boolean A" or "boolean B");
-  if (W && B); // Afirmacio 8
-  Serial.print("boolean A" or "boolean B");
-  if (W && A); // Afirmacio 9
-  Serial.print("boolean A" or "boolean B");
+Serial.print("g. Afirmacio 7: ((P > 0 && B) = ");
+          if(P > 0 && B) 
+          { 
+            Serial.println("true");
+          }                              // Afirmacio 7
+          else 
+          {
+            Serial.println("false");
+          }
+Serial.print("h. Afirmacio 8: (W && B) = ");
+          if(W && B) 
+          { 
+            Serial.println("true"); 
+          }                             // Afirmacio 8
+          else 
+          {
+            Serial.println("false"); 
+          }
+Serial.print("i. Afirmacio 9: (W && A) = ");
+          if(W && A) 
+          { 
+            Serial.println("true");
+          }                             // Afirmacio 9
+          else 
+          {
+            Serial.println("false");
+          }
 }
 
 void loop() {
