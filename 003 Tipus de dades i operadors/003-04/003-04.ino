7f95afbc4dd7e26a089ede7a8d5203046acc230f
GerardMaidana2024 <157707876+GerardMaidana2024@users.noreply.github.com>
2024-02-01 13:50:33 +0100

    Create 003-04.ino

---
+++ 003 Tipus de dades i operadors/003-04/003-04.ino
@@ -0,0 +1,45 @@
+/**********************************************************************************
+**                                                                               **
+**                          Control Structures                                   **
+**                                                                               **
+**   Data:01/02/2023                                      Nom: GerardMaidana     **
+**********************************************************************************/
+
+//********** Includes *************************************************************
+
+//********** Variables ************************************************************
+int tempAigua = 101;
+
+//********** Setup ****************************************************************
+void setup()              // run once, when the sketch starts
+{
+  Serial.begin(9600);     // set up Serial library at 9600 bps
+  delay(2000);
+    if (tempAigua < 90)
+      {
+        Serial.println("Aigua encara no bull");
+      }
+     else if(tempAigua >100)
+      { 
+        Serial.println("Aigua bullint!");
+      }
+     else if (tempAigua == 100)
+      {
+        Serial.println("Aigua a 100ºC");
+      }
+     else if (tempAigua >= 90 || tempAigua < 100)
+      {
+        Serial.println("aigua a punt de bullir");
+      }
+    else
+  {
+   
+  }
+}
+
+//********** Loop *****************************************************************
+void loop()   // we need this to be here even though its empty
+{
+}
+
+//********** Funcions *************************************************************
